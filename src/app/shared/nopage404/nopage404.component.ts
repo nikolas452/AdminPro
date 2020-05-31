4f0f8ca1c5481a8c485a1bee95b19ab92fb3b8ac
Nicolas Nitri <nitri.nicolas@gmail.com>
2020-05-31 20:43:49 -0300

    Primer commit

---
+++ src/app/shared/nopage404/nopage404.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopage404',
+  templateUrl: './nopage404.component.html',
+  styles: [
+  ]
+})
+export class Nopage404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
